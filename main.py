2636f0630f42e1be130fe91261f68bb5c54a98b4
Sriram1000 <148287789+Sriram1000@users.noreply.github.com>
2024-09-11 13:46:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
 import turtle as turtl
+import random
+
 painter = turtl.Turtle()
 screen = turtl.Screen()
-painter = turtl.Turtle()
 
 
+square_amount = screen.textinput("How many squares do you want?", "Enter a number:")
+square_amount = int(square_amount)
 
-screen.setup(width = 800, height = 800)
 painter.speed(0)
 painter.penup()
 painter.goto(-15, 15)
-
 painter.pendown()
+painter.pensize(5)
 
-square_amount = int(input("How many squares do you want?  "))
+x=30
 
 for i in range(square_amount):
     for i in range(4):
-        painter.forward(30)
+        painter.forward(x)
         painter.right(90)
     painter.penup()
-    painter.forward(10)
-    painter.right(90)
+    painter.left(90)
+    painter.forward(30)
+    painter.left(90)
+    painter.forward(30)
+    painter.right(180)
+    painter.pendown()
+    x=x+60
+    colors = ["red", "green", "blue", "yellow", "purple", "orange"]
+    y=random.choice(colors)
+    painter.color(y)
 
 
-wn = turtl.Screen()
-wn.mainloop()
+screen.mainloop()
