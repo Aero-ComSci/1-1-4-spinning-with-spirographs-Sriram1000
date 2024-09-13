da347830699207ddf5613b454ad3c73fdc81e0af
Sriram1000 <148287789+Sriram1000@users.noreply.github.com>
2024-09-12 20:49:55 -0700

    Update Concentric_squares.py

--- Concentric_squares.py
+++ Concentric_squares.py
@@ -8,17 +8,21 @@ screen = turtl.Screen()
 square_amount = screen.textinput("How many squares do you want?", "Enter a number:")
 square_amount = int(square_amount)
 
+painter.screen.bgcolor("light blue")
 painter.speed(0)
 painter.penup()
 painter.goto(-15, 15)
 painter.pendown()
 painter.pensize(5)
 
-x=30
+SquareSideLength=30
 
 for i in range(square_amount):
+    colors = ["red", "green", "blue", "yellow", "purple", "orange"]
+    PaintColor=random.choice(colors)
+    painter.color(PaintColor)
     for i in range(4):
-        painter.forward(x)
+        painter.forward(SquareSideLength)
         painter.right(90)
     painter.penup()
     painter.left(90)
@@ -27,10 +31,7 @@ for i in range(square_amount):
     painter.forward(30)
     painter.right(180)
     painter.pendown()
-    x=x+60
-    colors = ["red", "green", "blue", "yellow", "purple", "orange"]
-    y=random.choice(colors)
-    painter.color(y)
+    SquareSideLength += 60
 
 
 screen.mainloop()
